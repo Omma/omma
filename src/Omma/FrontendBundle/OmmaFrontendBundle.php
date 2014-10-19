b89c780c3cd4bc641fd4857cea4317ed68f69675
Florian Pfitzer <pfitzer@w3p.cc>
2014-10-19 23:23:49 +0200

    frontend bundle

---
+++ src/Omma/FrontendBundle/OmmaFrontendBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Omma\FrontendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OmmaFrontendBundle extends Bundle
+{
+}
---
+++ src/Omma/FrontendBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Omma\FrontendBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello World")')->count() > 0);
+    }
+}
