482b90a1a5da3f0b3caabee756dac323cb16fb7a
Florian Pfitzer <pfitzer@w3p.cc>
2014-11-20 12:18:28 +0100

    meeting rest controller

--- src/Omma/AppBundle/Controller/MeetingController.php
+++ src/Omma/AppBundle/Controller/MeetingController.php
@@ -4,6 +4,9 @@ namespace Omma\AppBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use Omma\AppBundle\Entity\Meeting;
+use Omma\AppBundle\Form\Type\MeetingForm;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  *
@@ -15,6 +18,26 @@ class MeetingController extends FOSRestController implements ClassResourceInterf
     public function cgetAction()
     {
         return $this->get("omma.app.manager.meeting")->findAll();
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Symfony\Component\Form\Form
+     */
+    public function cpostAction(Request $request)
+    {
+        $form = $this->createForm(new MeetingForm(), null, array(
+            "method" => "POST",
+            "csrf_protection" => false,
+        ));
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            return $this->view($form->getData());
+        }
+
+        return $this->view($form, 400);
     }
 
     public function getAction(Meeting $meeting)
---
+++ src/Omma/AppBundle/Form/Type/MeetingForm.php
@@ -0,0 +1,36 @@
+<?php
+namespace Omma\AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ *
+ *
+ * @author Florian Pfitzer <pfitzer@w3p.cc>
+ */
+class MeetingForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add("name", "text", array(
+                "constraints" => array(new NotBlank())
+            ))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            "data_class" => "Omma\AppBundle\Entity\Meeting"
+        ));
+    }
+
+    public function getName()
+    {
+        return "meeting";
+    }
+}
