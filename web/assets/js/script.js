8be94d9f3954894ad94924a1a67ea86782a20cbf
Jo Höhn <johannes.hoehn@hof-university.de>
2014-11-25 15:21:50 +0100

    typeahead search

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -114,74 +114,47 @@ $(document).ready(function() {
     var substringMatcher = function(strs) {
         return function findMatches(q, cb) {
 
+            if(q != '') {
+                $.getJSON("/web/temp_jsons/typeahead.json?q=" + q, function (data) {
 
+                    var matches = [];
 
+                    var substrRegex = new RegExp(q, 'i');
 
+                    $.each(data, function (i, str) {
 
-           /* $('input.typeahead').typeahead({
-                source: function (query, process) {
-                    source: substringMatcher(states)
-                }
+                        if (substrRegex.test(str.title)) {
+                            var value = "<a href=\"" + str.url + "\">";
+                            value += str.title + "<br />";
+                            value += "<small>";
+                            value += moment(str.date).format("DD.MM.YYYY [um] HH:mm");
+                            value += "</small></a>";
+                            matches.push({value: value});
+                        }
+                    });
 
-            });*/
+                    cb(matches);
 
-           // $('input.typeahead').typeahead().source = s;
-            //$('input.typeahead').data('ttTypeahead').dropdown.datasets[0].source = s
+                });
+            }
 
-
-            var matches, substringRegex;
-
-            matches = [];
-
-            var substrRegex = new RegExp(q, 'i');
-
-            $.each(strs, function(i, str) {
-                if (substrRegex.test(str)) {
-                    matches.push({ value: str });
-                }
-            });
-
-            cb(matches);
         };
     };
 
-    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-        'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-        'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-        'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-        'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-    ];
-
-    var s = ['North Dakota',
-        'Ohio', 'Oklahoma', 'Ocregon', 'Pennsylvania', 'Rhode Island',
-        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-    ];
-
-    //var source = $.getJSON( "/web/temp_jsons/todos-left-col.json?start="+get_current_month().start, function( data ) {
-    function typeahead_get_events() {
-        return "";
-    }
-
-
-
-
+    var events = [];
 
     $('input.typeahead').typeahead(
         {
-        hint: true,
-        highlight: true,
-        minLength: 1
-    },
-    {
-        name: 'states',
-        displayKey: 'value',
-        source: substringMatcher(states)
-    });
+            hint: true,
+            highlight: true,
+            minLength: 1
+        },
+        {
+            name: 'events',
+            displayKey: 'value',
+            source: substringMatcher(events)
+        }
+    );
 
 
 
