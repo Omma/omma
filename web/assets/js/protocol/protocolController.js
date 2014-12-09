91c3215a42c59cbabea530bb9729fffc0bc35b2c
Jo Höhn <johannes.hoehn@hof-university.de>
2014-12-09 17:01:15 +0100

    protocol textarea

--- web/assets/js/protocol/protocolController.js
+++ web/assets/js/protocol/protocolController.js
@@ -1,7 +1,13 @@
-/*angular.module('ommaApp').controller('protocolController', ['$scope', function ($scope) {
- alert('geht');
+
+
+
+
+
+
+angular.module('ommaApp').controller('protocolController', ['$scope', function ($scope) {
+ //alert('geht');
  $scope.orightml = 'test hallo 123';
  $scope.htmlcontent = $scope.orightml;
  $scope.disabled = false;
 
- }]);*/
+ }]);
