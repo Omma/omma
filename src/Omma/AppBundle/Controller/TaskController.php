b0dcacc1c6e9e40bb6c4474e7ab3150492fd60ed
Florian Pfitzer <pfitzer@w3p.cc>
2014-12-17 14:36:05 +0100

    task rest api

---
+++ src/Omma/AppBundle/Controller/TaskController.php
@@ -0,0 +1,58 @@
+<?php
+namespace Omma\AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use Omma\AppBundle\Entity\Task;
+use Omma\AppBundle\Form\Type\MeetingTaskForm;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ *
+ *
+ * @author Florian Pfitzer <pfitzer@w3p.cc>
+ */
+class TaskController extends FOSRestController implements ClassResourceInterface
+{
+    public function cgetAction()
+    {
+        return $this->get("omma.app.manager.task")->findBy(array(
+            "user" => $this->getUser()
+        ));
+    }
+
+    public function getAction(Task $task)
+    {
+        return $task;
+    }
+
+    public function putAction(Request $request, Task $task)
+    {
+        return $this->processForm($request, $task);
+    }
+
+    public function deleteAction(Task $task)
+    {
+        $this->get("omma.app.manager.task")->delete($task);
+
+        return $this->view("");
+    }
+
+    protected function processForm(Request $request, Task $task)
+    {
+        $new = null === $task->getId();
+        $form = $this->createForm(new MeetingTaskForm(), $task, array(
+            "method"          => $new ? "POST" : "PUT",
+            "csrf_protection" => false,
+        ));
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $this->get("omma.app.manager.task")->save($task);
+
+            return $this->view($task);
+        }
+
+        return $this->view($form, 400);
+    }
+}
